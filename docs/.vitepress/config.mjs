d745c055260dd64b5db1eb39e7d7565a4e050b1b
wu_hao <wuhaotdcq@outlook.com>
2025-12-28 12:56:30 +0800

    增加搜索

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -13,7 +13,16 @@ export default defineConfig({
     nav,
     head,
     sidebar: generateSidebar(),
-
+    search: {
+          placeholder: '搜索文档', // 搜索框占位符（默认：Search）
+          ariaLabel: '搜索文档', // 无障碍标签（默认：Search）
+          // 搜索结果无匹配时的提示（默认：No results found for "{query}"）
+          noResultsText: '未找到 "{query}" 相关结果',
+          // 搜索结果标题（默认：Results for "{query}"）
+          resultsText: '{count} 条 "{query}" 相关结果',
+          // 搜索加载中（默认：Loading...）
+          loadingText: '加载中...'
+        },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
       { icon: 'bilibili', link: 'https://space.bilibili.com/66129848' },
