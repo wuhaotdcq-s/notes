051de5ccc379cc51e19dfd096dade151f25a3b31
wu_hao <wuhaotdcq@outlook.com>
2025-12-30 15:07:43 +0800

    Revert "重新生成-20251228-2"
    This reverts commit 14ada92d485b2b43e4ac5ce3ce607327655477cf.

--- docs/.vitepress/head.mjs
+++ docs/.vitepress/head.mjs
@@ -1,4 +1,4 @@
 export default [
     ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css' }],
     ['script', { src: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.js' }],
-  ];
+  ],
--- docs/考试/一级建造师/工程经济/index.md
+++
@@ -1,7 +0,0 @@
----
-# https://vitepress.dev/reference/default-theme-home-page
-title: 工程经济
-layout: doc
-order: 3
----
-## 工程经济
