c2f3fe8ff8c10d9507733b07490833eb19a9a24e
wu_hao <wuhaotdcq@outlook.com>
2025-12-30 14:34:35 +0800

    重新生成-20251230-1

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,16 @@
+// index.js
+// import DefaultTheme from 'vitepress/theme'
+// import './custom.css'
+
+// export default DefaultTheme
+
+// .vitepress/theme/index.js
+
+// 可以直接在主题入口导入 Vue 文件
+// VitePress 已预先配置 @vitejs/plugin-vue
+// import Layout from './MyLayout.vue';
+import './style.css'
+// import './custom.css'
+export default {
+
+};
